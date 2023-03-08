3ed813fb3935d406dcc2db6f4f72942c868e24c6
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2023-03-08 10:14:37 +0100

    changed the function line code to be for 8 sensor instead of 6

--- Functions/line/line.ino
+++ Functions/line/line.ino
@@ -2,7 +2,7 @@
 
 
 QTRSensors qtr;
-const uint8_t SensorCount = 6; //amount of sensors you wanna use, this is on 6 cus im using only 6 of the 8 atm.
+const uint8_t SensorCount = 8; //amount of sensors you wanna use, this is on 6 cus im using only 6 of the 8 atm.
 uint16_t sensorValues[SensorCount];
 
 
@@ -10,18 +10,22 @@ void setup()
 {
  // configure the sensors
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
 
   // analogRead() takes about 0.1 ms on an AVR.
-  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++){
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 8 sensors
+  // * 10 reads per calibrate() call = ~32 ms per calibrate() call.
+  // Call calibrate() 15 times to make calibration take about 0,48 seconds.
+  
+  Motor(255,255,0,0); //makes it move forward while calibrating
+
+  for (uint16_t i = 0; i < 15; i++){
     qtr.calibrate();
-  }
+  }  
+  
   digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
   // print the calibration minimum values measured when emitters were on
@@ -55,64 +59,88 @@ void loop()
   //The the data most on the right is the left sensor, and vice versa
   for (uint8_t i = 0; i < SensorCount; i++){
     Serial.print(sensorValues[i]);
-//    if(i< SensorCount+1){
-//    Serial.print(i+1);}
-    Serial.print('\t');
+  //    if(i< SensorCount+1){
+  //    Serial.print(i+1);}
+      Serial.print('\t');
+    }
+    Serial.println(position);
+  //  delay(100);
+  
+  //0 is the most right sensor
+  //7 is the most left sensor
+
+  if(sensorValues[0] > 600 && sensorValues[1] > 600 && sensorValues[2] > 600 && sensorValues[3] > 600 && sensorValues[4] > 600 && sensorValues[5] > 600 && sensorValues[6] > 600 && sensorValues[7] > 600 && gripperClosed == 0){
+    //checks whether every sensor sees black and whether or not the gripper is already closed or not. if so, stop moving and close the gripper (to pick up the block thingy)
+    Stop();
+    //close gripper
+    gripperClose();
+    gripperClosed = 1;
+   
+    delay(200);
+    Right();
+    delay(100);
   }
-  Serial.println(position);
-//  delay(100);
-
-//0 is the most right sensor
-//5 is the most left sensor
-
-
-
-//Line following
-if(sensorValues[0] < 600 && sensorValues[1] < 600 && sensorValues[2] < 600 && sensorValues[3] < 600 && sensorValues[4] < 600 && sensorValues[5] < 600){
-  if(lastSensor == 1){
-//      Right();
-      Motor(0, 255, 255, 0); //this is a function form the motor.ino file, so if you wanna use your own motor control, delete this. 
+  
+  //Line following
+  else if(sensorValues[0] < 600 && sensorValues[1] < 600 && sensorValues[2] < 600 && sensorValues[3] < 600 && sensorValues[4] < 600 && sensorValues[5] < 600 && sensorValues[6] < 600 && sensorValues[7] < 600){
+    if(lastSensor == 1){
+      Motor(0, 210, 200, 0);
       Serial.print("AAHHHH WHERE IS IT !!!!!!!!!!!!!!!! GOO RRIGGHHTHTHTHT!!!!!!");
+    }
+    
+    else if (lastSensor == 7){
+      Motor(210, 0, 0, 200);
+      Serial.print("AAHHHH WHERE IS IT !!!!!!!!!!!!!!!! GOO LEFFFTT!!!!");
+    }
   }
-  else if (lastSensor == 4){
-//     Left();
-     Motor(255, 0, 0, 255);
-     Serial.print("AAHHHH WHERE IS IT !!!!!!!!!!!!!!!! GOO LEFFFTT!!!!");
+    
+  else if(sensorValues[3] > 600 && sensorValues[4] > 600){
+    Forward();
+    Serial.print("go forward");
   }
-}
-else if (sensorValues[0] > 600){
-    Motor(100, 255, 0, 0);
-//    Right();
+    
+  else if(sensorValues[3] > 600){
+    Motor(250, 255, 0, 0);
+    Serial.print("go forwardish");
+  }
+  
+  else if(sensorValues[4] > 600){
+    Motor(255, 245, 0, 0);
+    Serial.print("go forwardish");
+  }
+    
+  else if (sensorValues[7] > 600){
+    Motor(255, 180, 0, 0);
+    Serial.print("Go hard left");
+    lastSensor = 7;
+  }
+    
+  else if(sensorValues[6] > 600){
+    Motor(255, 210, 0, 0);
+    Serial.print("Go left");
+    lastSensor = 7;
+  }
+  
+  else if(sensorValues[5] > 600){
+    Motor(255, 230, 0, 0);
+    Serial.print("go leftish");
+  }
+  
+  else if(sensorValues[2] > 600){
+    Motor(230, 255, 0, 0);
+    Serial.print("go rightish");
+  }
+  
+  else if(sensorValues[1] > 600){
+     Motor(210, 255, 0, 0);
+     Serial.print("Go right");
+     lastSensor = 1;
+  }
+  
+  else if (sensorValues[0] > 600){
+    Motor(180, 255, 0, 0);
     Serial.print("Go hard right");
     lastSensor = 1;
-  }
-else if(sensorValues[1] > 600){
-   Motor(150, 255, 0, 0);
-   Serial.print("Go right");
-   lastSensor = 1;
-}
-else if(sensorValues[4] > 600){
-  Motor(255, 150, 0, 0);
-  Serial.print("Go left");
-   lastSensor = 4;
-}
-else if(sensorValues[5] > 600){
-//  Left();
-   Motor(255, 100, 0, 0);
-   Serial.print("Go hard left");
-   lastSensor = 4;
-  }
-else if(sensorValues[2] > 600 && sensorValues[3] > 600){
-  Forward();
-  Serial.print("go forward");
-  }
-else if(sensorValues[2] > 600){
-  Motor(255, 220, 0, 0);
-  Serial.print("go forwardish");
-  }
-else if(sensorValues[3] > 600){
-  Motor(220, 255, 0, 0);
-  Serial.print("go forwardish");
   }
 
 }
