ceeb9d79aa2939a47b9a72795e13f40e5087fe70
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2023-03-22 13:49:40 +0100

    Changed to remove library dependency

--- Gripper.ino
+++ Gripper.ino
@@ -1,34 +1,59 @@
-#include <Servo.h>
-Servo gripper;
-
-const int gripperPin = 3;
+const int gripperPin = 12;
+const int rotatorPin = 13;
 
 int servoPos = 0;
+int rotatorPos = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  gripper.attach(gripperPin);
+  pinMode(gripperPin, OUTPUT);
+  pinMode(rotatorPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  gripperClose();
-  delay(1000);
-  gripperOpen();
-  delay(1000);
+    sensorLeft();
+    delay(1000);
+    sensorCenter();
+    gripperOpen();
+    delay(1000);
+    sensorRight();
+    delay(1000);
+    sensorCenter();
+    gripperClose();
+    delay(1000);
 }
 
 void gripperOpen(){
-  for (servoPos = 0; servoPos <= 180; servoPos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    gripper.write(servoPos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(1600); // Duration of the pulse in microseconds
+    digitalWrite(gripperPin, LOW);
+    // Pulses duration: 1600 = open
 }
 
 void gripperClose(){
-  for (servoPos = 180; servoPos >= 0; servoPos -= 1) { // goes from 180 degrees to 0 degrees
-    gripper.write(servoPos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(1010); // Duration of the pulse in microseconds
+    digitalWrite(gripperPin, LOW);
+    // Pulses duration: 1000 = fully closed
+}
+
+void sensorRight(){
+    digitalWrite(rotatorPin, HIGH);
+    delayMicroseconds(350); // Duration of the pusle in microseconds
+    digitalWrite(rotatorPin, LOW);
+    //Pulse duration: 350ms = 0 degrees
+}
+
+void sensorLeft(){
+    digitalWrite(rotatorPin, HIGH);
+    delayMicroseconds(2310); // Duration of the pusle in microseconds
+    digitalWrite(rotatorPin, LOW);
+    //Pulse duration: 2310ms - 180deg
+}
+
+void sensorCenter(){
+    digitalWrite(rotatorPin, HIGH);
+    delayMicroseconds(1310); // Duration of the pusle in microseconds
+    digitalWrite(rotatorPin, LOW);
+    //Pulse duration: 1310ms = 90 degrees
 }
