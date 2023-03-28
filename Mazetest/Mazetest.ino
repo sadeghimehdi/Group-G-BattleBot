500945ac0134ce3725f3b4215a38ed3fad092580
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2023-03-28 17:10:33 +0200

    Adapted ending code for maze

--- Mazetest/Mazetest.ino
+++ Mazetest/Mazetest.ino
@@ -1,3 +1,22 @@
+#include <QTRSensors.h>
+QTRSensors qtr;
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
+/****************************************************************************
+ ***                             Millis                                   ***
+ ****************************************************************************/
+unsigned long previousMillis_1 = 0
+const long interval_1 = 5000; //interval for first event (5seconds)
+int theEnd = 0;
+int lastSensor = NULL;
+
+
+
+boolean mazeComplete = false;
+
+
+
 const int gripperPin = 12;
 const int rotatorPin = 13;
 int servoPos = 0;
@@ -635,3 +654,229 @@ void sensorCenter(){
     delay(20);
   }
 }
+
+void startupSequence(){
+
+}
+
+void lineFollowAndEnd(){
+
+/****************************************************************************
+ ***                          Line Sensor with Motor                      ***
+ ****************************************************************************/
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  //The the data most on the right is the left sensor, and vice versa
+  for (uint8_t i = 0; i < SensorCount; i++){
+    Serial.print(sensorValues[i]);
+//    if(i< SensorCount+1){
+//    Serial.print(i+1);}
+    Serial.print('\t');
+  }
+  Serial.println(position);
+
+//if(sensorValues[0] > 800 || sensorValues[1] > 800 || sensorValues[2] > 800 || sensorValues[3] > 800 || sensorValues[4] > 800 || sensorValues[5] > 800 || sensorValues[6] > 800 || sensorValues[7] > 800){
+  int amount = 0;
+
+  if(sensorValues[0] > 800){ amount = amount + 1;};
+  if(sensorValues[1] > 800){ amount = amount + 1;};
+  if(sensorValues[2] > 800){ amount = amount + 1;};
+  if(sensorValues[3] > 800){ amount = amount + 1;};
+  if(sensorValues[4] > 800){ amount = amount + 1;};
+  if(sensorValues[5] > 800){ amount = amount + 1;};
+  if(sensorValues[6] > 800){ amount = amount + 1;};
+  if(sensorValues[7] > 800){ amount = amount + 1;};
+  Serial.println("The amount is" + String(amount));
+  
+
+
+/****************************************************************************
+ ***                       Pulse and Stop & Drop                          ***
+ ****************************************************************************/
+
+//so when its at the end
+unsigned long currentMillisRight = millis();
+if(amount == 8 && theEnd == 0){
+  if(currentMillisRight - previousMillis_1 >= interval_1){
+    PulseCountRight = 0;   
+    int previousPulseState = digitalRead(PulsePinRight);
+    unsigned long lastPulseTime = millis();
+    Motor(200,200,0,0);
+    
+    while (1)
+    {
+      Serial.println(PulseCountRight);
+      int pulseState = digitalRead(PulsePinRight);
+      
+      if (pulseState != previousPulseState)
+      {
+        // State change
+        previousPulseState = pulseState;
+        PulseCountRight++;
+        lastPulseTime = millis();
+      }
+      
+      if (PulseCountRight == 7)
+      {
+        //stop after a set amount of pulses
+        Serial.println("STOP AHHHH");
+        theEnd = 1;
+        return;
+      }
+    }
+  }
+}
+
+if(theEnd == 1 && amount == 8){
+    stopRobot();
+    gripperOpen();
+    delay(100);
+    
+    moveBackwards();
+    waitUntilPulseCount(40);
+    stopRobot();
+    gripperClose();
+
+    mazeComplete = true;                                                                //END OF PROGRAM VARIABLE, PUT EVERYTHING IN THE LOOP() IN AN if(!mazeComplete) statement
+    
+}else if (theEnd == 1 && amount < 8)
+{ 
+  theEnd = 0;
+}
+
+
+
+
+/****************************************************************************
+ ***                         Line folllowing                             ***
+ ****************************************************************************/
+ else if(sensorValues[0] < 600 && sensorValues[1] < 600 && sensorValues[2] < 600 && sensorValues[3] < 600 && sensorValues[4] < 600 && sensorValues[5] < 600 && sensorValues[6] < 600 && sensorValues[7] < 600){
+  if(lastSensor < 4){
+//      Right turn
+      Motor(0, 190, 190, 0);
+      Serial.print("AAHHHH WHERE IS IT !!!!!!!!!!!!!!!! GOO RRIGGHHTHTHTHT!!!!!!");
+  }
+  else if (lastSensor > 3){
+//     Left turn
+     Motor(190, 0, 0, 190);
+     Serial.print("AAHHHH WHERE IS IT !!!!!!!!!!!!!!!! GOO LEFFFTT!!!!");
+  }
+
+ }  
+  else if (sensorValues[7] > 600 && sensorValues[6] < 600){
+    Motor(200, 100, 0, 0);
+    Serial.print("Go hard left");
+    lastSensor = 7;
+    Serial.println("Last sensor is " + String(lastSensor));
+  }
+  else if (sensorValues[7] > 600 && sensorValues[6] > 600){
+    Motor(200, 110, 0, 0);
+    Serial.print("Go hardish left");
+    lastSensor = 7;
+    Serial.println("Last sensor is " + String(lastSensor));
+  }
+  
+  else if(sensorValues[6] > 600 && sensorValues[5] < 600){
+     Motor(200, 120, 0, 0);
+     Serial.println("Go left");
+     lastSensor = 6;
+     Serial.println("Last sensor is " + String(lastSensor));
+  }
+  else if(sensorValues[6] > 600 && sensorValues[5] > 600){
+     Motor(200, 130, 0, 0);
+     Serial.println("Goish left");
+     lastSensor = 6;
+     Serial.println("Last sensor is " + String(lastSensor));
+  }
+
+  
+  else if(sensorValues[5] > 600 && sensorValues[4] < 600){
+    Motor(200, 130, 0, 0);
+    Serial.println("go leftish");
+    lastSensor = 5;
+    Serial.println("Last sensor is " + String(lastSensor));
+    }
+
+      else if(sensorValues[5] > 600 && sensorValues[4] > 600){
+    Motor(200, 140, 0, 0);
+//  Motor(230, 170, 0, 0);
+    Serial.println("go leftish"); 
+    lastSensor = 5;
+    Serial.println("Last sensor is " + String(lastSensor));
+    }
+
+    else if (sensorValues[0] > 600 && sensorValues[1] < 600){
+      Motor(100, 200, 0, 0);
+      Serial.println("Go hard right");
+      lastSensor = 0;
+      Serial.println("Last sensor is " + String(lastSensor));
+    }
+
+ else if(sensorValues[0] > 600 && sensorValues[1] > 600){
+     Motor(110, 200, 0, 0);
+     Serial.println("Go right");
+     lastSensor = 1;
+     Serial.println("Last sensor is " + String(lastSensor));
+  }
+    
+  else if(sensorValues[1] > 600 && sensorValues[2] < 600){
+     Motor(120, 200, 0, 0);
+     Serial.println("Go right");
+     lastSensor = 1;
+     Serial.println("Last sensor is " + String(lastSensor));
+  }
+ 
+    else if(sensorValues[1] > 600 && sensorValues[2] > 600){
+    Motor(120, 200, 0, 0);
+    Serial.println("go rightish");
+    lastSensor = 2;
+    Serial.println("Last sensor is " + String(lastSensor));
+  }
+  
+  else if(sensorValues[2] > 600 && sensorValues[3] < 600){
+    Motor(130, 200, 0, 0);
+    Serial.println("go rightish");
+    lastSensor = 2;
+    Serial.println("Last sensor is " + String(lastSensor));
+  }
+
+  else if(sensorValues[2] > 600 && sensorValues[3] > 600){
+    Motor(140, 200, 0, 0);
+//Motor(170, 230, 0, 0);
+    Serial.println("go rightish");
+    lastSensor = 2;
+    Serial.println("Last sensor is " + String(lastSensor));
+  }
+
+  else if(sensorValues[3] > 600 && sensorValues[4] > 600){
+//    Forward();
+//    Motor(170, 170, 0, 0);
+    Motor(250, 250, 0, 0);
+    Serial.print("go forward");
+//    lastSensor = NULL;
+    } 
+    
+  else if(sensorValues[3] > 600 && sensorValues[4] < 600){
+    Motor(170, 180, 0, 0);
+//  Motor(240, 250, 0, 0);
+//    Forward();
+
+    Serial.print("go forwardish");
+    lastSensor = 3;
+  }
+  
+  else if(sensorValues[4] > 600 && sensorValues[3] < 600){
+    Motor(180, 170, 0, 0);
+//        Motor(250, 240, 0, 0);
+//    Forward();
+
+    Serial.print("go forwardish");
+    lastSensor = 4;
+    }  
+
+}//end linefollowandend
